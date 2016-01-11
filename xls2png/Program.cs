c55d73a84927902f824b47d2830a7d5c5337b8dc
aki <wiki908.zh@gmail.com>
2016-01-12 07:32:54 +0800

    generalize

--- xls2png/Program.cs
+++ xls2png/Program.cs
@@ -9,6 +9,12 @@ namespace xls2png
         [STAThread]
         static void Main(string[] args)
         {
+            if(args.Length != 4 && args.Length != 6)
+            {
+                //                         0        1           2       3          4      5
+                Console.WriteLine("xls2png <in.xls> <sheetname> <range> <out.png> [<unit> <addval>]");
+                return;
+            }
             // open file
             Console.WriteLine("xls2png: please wait...");
 
@@ -20,19 +26,22 @@ namespace xls2png
                 var wb = eapp.Workbooks.Open(args[0]);
 
                 // read sheet
-                Excel.Worksheet sht = wb.Sheets[4];
+                Excel.Worksheet sht = wb.Sheets[args[1]];
                 sht.Select();
 
-                // set times, will auto calculate
-                sht.get_Range("C29").Value = sht.get_Range("C29").Value + 1;
-
+                if (args.Length == 6)
+                {
+                    // set times, will auto calculate
+                    sht.get_Range(args[4]).Value = sht.get_Range(args[4]).Value + int.Parse(args[5]);
+                }
+                
                 // copy pic
-                Excel.Range rng = sht.get_Range("H4:M12");
+                Excel.Range rng = sht.get_Range(args[2]);
                 rng.CopyPicture(Excel.XlPictureAppearance.xlScreen, Excel.XlCopyPictureFormat.xlBitmap);
 
                 // export pic
                 var img = Clipboard.GetImage();
-                if (img != null) { img.Save(args[1]); img.Dispose(); }
+                if (img != null) { img.Save(args[3]); img.Dispose(); }
                 else { Console.WriteLine("xls2png: image not copied"); }
 
                 // close file                
